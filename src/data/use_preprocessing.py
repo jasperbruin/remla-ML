98f9c0db21d7558c06377e1d24f4b849b612e3ed
Jasper Bruin <jasperbruin@me.com>
2024-05-08 13:07:40 +0200

    Resolved flake8 issue

--- src/data/use_preprocessing.py
+++ src/data/use_preprocessing.py
@@ -7,8 +7,10 @@ Script that uses the preprocessing functions from lib_ml.
 
 from lib_ml.preprocessing import preprocess_data
 
+
 def main():
     preprocess_data()
+
 
 if __name__ == "__main__":
     main()
