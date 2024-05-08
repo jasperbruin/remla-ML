8cf068efa98d86085d8bbd6dedbed8242673ae8b
Jasper Bruin <jasperbruin@me.com>
2024-05-08 13:03:09 +0200

    Updated the preprocessing part

--- src/data/use_preprocessing.py
+++ src/data/use_preprocessing.py
@@ -1,4 +1,10 @@
 # model-training/src/data/use_preprocessing.py
+# pylint: disable=C0116
+
+"""
+Script that uses the preprocessing functions from lib_ml.
+"""
+
 from lib_ml.preprocessing import preprocess_data
 
 def main():
