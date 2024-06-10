87e35eb4917267aa2c84c2d53ce5060f9fe8d026
AlexandruDumitriu <a.dumitriu-1@student.tudelft.nl>
2024-06-10 19:25:13 +0200

    update configuration

--- conftest.py
+++ conftest.py
@@ -8,7 +8,11 @@ def pytest_runtest_makereport(item, call):
     rep = outcome.get_result()
     if rep.when == 'call':
         capstdout = item.config.pluginmanager.getplugin('capturemanager').read_global_capture()
-        rep.capstdout = capstdout
+        if capstdout is not None:
+            if not hasattr(rep, 'capstdout'):
+                rep.capstdout = capstdout[0]
+            else:
+                rep.capstdout += capstdout[0]
 
 # Hook to modify the JSON report
 @pytest.hookimpl(tryfirst=True)
@@ -16,14 +20,28 @@ def pytest_sessionfinish(session, exitstatus):
     json_report_file = session.config.option.json_report_file
     if not json_report_file:
         return
-    
+
     with open(json_report_file, 'r', encoding='utf-8') as f:
         report_data = json.load(f)
 
     for entry in report_data.get('tests', []):
         nodeid = entry['nodeid']
-        report = session.config._json_report.report[nodeid]
-        entry['captured_output'] = report.capstdout
+        if nodeid in session.config._json_report.report:
+            report = session.config._json_report.report[nodeid]
+            entry['captured_output'] = getattr(report, 'capstdout', '')
 
     with open(json_report_file, 'w', encoding='utf-8') as f:
         json.dump(report_data, f, indent=4)
+
+@pytest.hookimpl(tryfirst=True)
+def pytest_configure(config):
+    config._json_report = type('', (), {'report': {}})()
+
+@pytest.hookimpl(tryfirst=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    rep = outcome.get_result()
+    config = item.config
+    if not hasattr(config._json_report, 'report'):
+        config._json_report.report = {}
+    config._json_report.report[item.nodeid] = rep
